6e0d198df0c031b0b6c6d68c1b98786f64013d68
Spyros Magripis <mechoulas@gmail.com>
2016-02-14 13:00:05 +0100

    second class added

--- calculate.php
+++ calculate.php
@@ -7,10 +7,57 @@
     </head>
 <body>
 
+
+<div id="form">
+
+        <form action= "calculate.php" method="get">
+            Enter breadth[m]:<input type="text" name="breadthListItem"><br>
+            Enter height[m]: <input type="text" name="heightListItem"><br>
+            Enter length[m]: <input type="text" name="lengthListItem"/>
+        
+           <select name="action">
+            <option>Rectangular</option>
+            <option>TriangularBeam</option>
+        </select>
+        
+        <input type="submit" name="submit" value="Calculate Now">
+        
+        </form>
+</div><br/>
+
+  <button id="button1">Show me the Beam!</button>
+  <form>
+    <input type="number" value="4" name="test">
+  </form
+ <br/>
+
+
 <?php
 
-        // The code below creates the class
-        class Beam {
+
+
+if(isset($_GET["submit"])){
+
+    $action = $_GET['action'];
+    #echo "line 32,";
+    
+    if($action=="Rectangular"){
+        // Creating a new beam with varriables as given by the user
+        $me = new RectangularBeam($_GET["breadthListItem"], $_GET["heightListItem"], $_GET["lengthListItem"]); 
+        // Printing out, what the report method returns
+        echo $me->report();      
+    } else{
+        $me = new TriangularBeam($_GET["breadthListItem"], $_GET["heightListItem"], $_GET["lengthListItem"]);
+        echo $me->report();
+        }
+            }
+    
+        
+    
+
+     
+     // The code below creates the class
+        class Section {
             // Creating some properties (variables tied to an object)
             //public $isAlive = true;
             public $breadth;
@@ -26,28 +73,30 @@
               $this->length = $length;
               //$this->YoungModulus = $YoungModulus;
             }
-                        
+             
+        }
+        
+        class RectangularBeam extends Section {
+        
             // Creating a method (function tied to an object)
             public function report() {
               $area = $this->breadth * $this->height;
-              return "The beam dimensions are " . $this->breadth . "m, " . $this->height . "m, " . $this->length. "m, ". $area . " m2 and " .$this->YoungModulus . "GPa .";
+              return "The beam dimensions are:<br> " . "Breadth: ". $this->breadth . "m<br> ". "Height: " . $this->height . "m<br> " . "Length: ". $this->length. "m<br>". "Area: ". $area . " m2<br>". "Young Modulus: " .$this->YoungModulus . "GPa";
             }
           }
           
-        // Creating a new beam with varriables as given by the user
-        $me = new Beam($_GET["breadthListItem"], $_GET["heightListItem"], $_GET["lengthListItem"]); 
-        // Printing out, what the report method returns
-        echo $me->report();
+          class TriangularBeam extends Section{
+            // Creating a method (function tied to an object)
+            public function report() {
+              $area =0.5 * $this->breadth * $this->height;
+              return "The beam dimensions are:<br> " . "Breadth: ". $this->breadth . "m<br> ". "Height: " . $this->height . "m<br> " . "Length: ". $this->length. "m<br>". "Area: ". $area . " m2<br>". "Young Modulus: " .$this->YoungModulus . "GPa";
+            }
+          
+          
+          }
         ?>
-        
-        </body>
-<body>
-  <button id="button1">Show me the Beam!</button>
-  <form>
-    <input type="number" value="4" name="test">
-  </form
- <br/>
-<div class="shapes"></div>
-  </div>
+ <div class="shapes"></div> <br/>
+  </div>       
+
 </body>
 </html>
